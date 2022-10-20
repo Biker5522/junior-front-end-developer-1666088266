c93b4a2d409f6e97cfec43310214a958ba3dd3b7
Dawid Cichy <38354498+Biker5522@users.noreply.github.com>
2022-10-21 00:37:56 +0200

    Browse Tasks

--- src/App.js
+++ src/App.js
@@ -3,97 +3,20 @@ import React, { useEffect, useState } from 'react'
 
 import Header from './components/Header'
 import TasksList from './components/TasksList'
+import BusinessContext from './components/BusinessContext'
 import moment from 'moment'
-
+import { Context } from './components/Context'
 function App() {
   useEffect(() => {}, [])
-  const [businessTasks, setBusinessTasks] = useState([
-    {
-      id: 1,
-      title: 'New Sprint, tasks and intro information',
-      body:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ',
-      isNew: true,
-      author: 'Olga Nelson',
-      created_at: Date.now(),
-      active: false,
-    },
-    {
-      id: 2,
-      title: 'Aplication has been accepted ',
-      body:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-      isNew: false,
-      author: 'Olga Nelson',
-      created_at: Date.now(),
-      active: true,
-    },
-    {
-      id: 3,
-      title: 'Style business contexts tab',
-      body:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad',
-      isNew: false,
-      author: 'Juan Pablo',
-    },
-  ])
-
+  const [context, setContext] = useState(null)
   return (
     <div className="App">
       <Header />
       <div className="mainView">
-        <TasksList />
-        <div className="businessContext">
-          <div className="headerMain">
-            <h2>BUSINESS CONTEXT</h2>
-          </div>
-          <div className="businessMain">
-            <div className="businessTasks">
-              {businessTasks.map((task, index) => {
-                if (task.isNew === true) {
-                  return (
-                    <div key={index} className="businessTask">
-                      <div>
-                        <div className="newTask">New</div>
-                        <p className="name">{task.author}</p>
-                      </div>
-                      <h3>{task.title}</h3>
-                      <p>{task.body.substring(0, 60)}...</p>
-                    </div>
-                  )
-                } else if (task.isNew === false) {
-                  if (task.active === true) {
-                    return (
-                      <div key={index} className="businessTask active">
-                        <p className="name">{task.author}</p>
-                        <h3>{task.title}</h3>
-                        <p>{task.body.substring(0, 60)}...</p>
-                      </div>
-                    )
-                  } else {
-                    return (
-                      <div key={index} className="businessTask unactive">
-                        <p className="name">{task.author}</p>
-                        <h3>{task.title}</h3>
-                        <p>{task.body.substring(0, 60)}...</p>
-                      </div>
-                    )
-                  }
-                } else return null
-              })}
-            </div>
-            <div className="businessBody">
-              <h3>{businessTasks[1].title}</h3>
-              <h4>
-                {businessTasks[1].author}
-                {moment(businessTasks[1].date).format(' Do MMMM')}
-                {'  '}
-                {moment(businessTasks[1].date).format('h:mm')}
-              </h4>
-              <p>{businessTasks[1].body}</p>
-            </div>
-          </div>
-        </div>
+        <Context.Provider value={[context, setContext]}>
+          <TasksList />
+          <BusinessContext />
+        </Context.Provider>
       </div>
     </div>
   )
--- src/components/TasksList.js
+++ src/components/TasksList.js
@@ -2,47 +2,57 @@ import Check from '../icons/check.png'
 import ToDo from '../icons/toDo.png'
 import Lock from '../icons/lock.png'
 import ArrowDown from '../icons/arrowDown.png'
-
-import React, { useEffect, useState } from 'react'
+import { Context } from './Context'
+import React, { useEffect, useState, useContext } from 'react'
 
 export default function TasksList() {
+  const [context, setContext] = useContext(Context)
   const [tasks, setTasks] = useState([
     {
       id: 1,
       title: 'Hay, Hello',
       status: 'done',
+      active: true,
     },
     {
       id: 2,
       title: 'Style Tasks List ',
       status: 'done',
+      active: false,
     },
     {
       id: 3,
       title: 'Style business contexts tab',
       status: 'inProgress',
+      active: false,
     },
     {
       id: 5,
       title: 'Student can browse tasks',
       status: 'inProgress',
+      active: false,
     },
     {
       id: 6,
       title: 'Stuned can rowse business contexts',
       status: 'locked',
-    },
-    {
-      id: 7,
-      title: 'Deploy App',
-      status: 'locked',
-    },
-    {
-      id: 8,
-      title: 'Thank You!',
-      status: 'locked',
+      active: false,
     },
   ])
+
+  const HandleClick = (task) => (e) => {
+    e.preventDefault()
+    if (task.active == false && task.status != 'locked') {
+      tasks.forEach((x) => {
+        x.active = false
+        console.log(x.active)
+      })
+      task.active = true
+      setContext(task)
+    }
+    console.log(task.active)
+  }
+
   return (
     <div className="tasksContainer">
       <div className="headerMain">
@@ -52,14 +62,22 @@ export default function TasksList() {
       {tasks.map((task, index) => {
         if (task.status === 'done') {
           return (
-            <div key={index} className="task taskDone">
+            <div
+              key={index}
+              className="task taskDone"
+              onClick={HandleClick(task)}
+            >
               <img className="taskIcon" src={Check} alt="Done" />
               <p>{task.title}</p>
             </div>
           )
         } else if (task.status === 'inProgress') {
           return (
-            <div key={index} className="task taskInProgress">
+            <div
+              key={index}
+              className="task taskInProgress "
+              onClick={HandleClick(task)}
+            >
               <img className="taskIcon" src={ToDo} alt="inProgress" />{' '}
               <p>{task.title}</p>
             </div>
