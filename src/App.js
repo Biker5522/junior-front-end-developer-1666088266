9e9c92383fddf0189bfd3876ad67b38d1cd1af5c
Dawid Cichy <38354498+Biker5522@users.noreply.github.com>
2022-10-21 07:48:36 +0200

    Browse business contexts

--- src/App.js
+++ src/App.js
@@ -2,21 +2,24 @@ import './styles/App.css'
 import React, { useEffect, useState } from 'react'
 
 import Header from './components/Header'
+import Footer from './components/Footer'
 import TasksList from './components/TasksList'
 import BusinessContext from './components/BusinessContext'
 import moment from 'moment'
 import { Context } from './components/Context'
 function App() {
   useEffect(() => {}, [])
-  const [context, setContext] = useState(null)
+  const [context, setContext] = useState(1)
   return (
     <div className="App">
       <Header />
       <div className="mainView">
+        {/*Set global task variable */}
         <Context.Provider value={[context, setContext]}>
           <TasksList />
           <BusinessContext />
         </Context.Provider>
+        <Footer />
       </div>
     </div>
   )
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './styles/index.css'
+import './styles/footer.css'
 import './styles/tasks.css'
 import './styles/main.css'
 import './styles/businessContext.css'
