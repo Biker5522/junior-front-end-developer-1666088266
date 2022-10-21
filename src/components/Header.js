6c894fb448a9bdcfec7eb4c2deb4626c5e95ca21
Dawid Cichy <38354498+Biker5522@users.noreply.github.com>
2022-10-21 08:29:48 +0200

    Deploy

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react'
-
+import Face from '../icons/face.png'
 export default function TasksList() {
   return (
     <div className="Header">
       <div className="headerText">
         nerds.family
-        <img src="https://media-exp1.licdn.com/dms/image/D4D35AQFC7wQJSDsnQw/profile-framedphoto-shrink_200_200/0/1665603729815?e=1666918800&v=beta&t=rXuaZ2nqFbaZdDdIDyKh49Fx6R8O0cR0y9LzwDVoRPw"></img>
+        <img src={Face} />
       </div>
     </div>
   )
